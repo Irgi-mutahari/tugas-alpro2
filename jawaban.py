e66a7e1b099946c389c099665b13df7a08b92582
Irgi-mutahari <115854644+Irgi-mutahari@users.noreply.github.com>
2022-11-09 18:08:18 +0700

    Create jawaban.py

---
+++ jawaban.py
@@ -0,0 +1,25 @@
+String = "Hello World"
+print(String)
+print(20*"-")
+
+Integer = 300
+print(Integer)
+print(20*"-")
+
+Float = 500.6
+print(Float)
+print(20*"-")
+
+List = ["ayah","ibu","anak"]
+print(List)
+print(20*"-")
+
+Boolean1 = True
+print(Boolean1)
+print(20*"-")
+
+Boolean2 = False
+print(Boolean2)
+print(20*"-")
+
+
