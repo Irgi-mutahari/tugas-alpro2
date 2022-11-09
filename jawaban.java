5a0adcb73932c12221d673f9e5b695029e8a67c2
Irgi-mutahari <115854644+Irgi-mutahari@users.noreply.github.com>
2022-11-09 17:41:32 +0700

    Create jawaban.java

---
+++ jawaban.java
@@ -0,0 +1,42 @@
+public class jawaban {
+public static void main (String [] args){
+//INTEGER
+byte value_byte = 80;
+short value_short = 4000;
+int value_int = 5000000;
+long value_long = 9000000000L;
+System.out.println("Nilai tipe data byte = " + value_byte);
+System.out.println("Nilai tipe data short = " + value_short);
+System.out.println("Nilai tipe data int = " + value_int);
+System.out.println("Nilai tipe data long = " + value_long);
+
+//BOOLEAN
+boolean Irgisangatkeren = true;
+boolean Irgisangatjelek = false;
+System.out.println(Irgisangatkeren);
+System.out.println(Irgisangatjelek);
+
+//CHAR
+char huruf_A = 'A', huruf_A_juga = 55;
+char huruf_B = 'B', huruf_B_juga = 77;
+char persen = '%', persen_juga = 89;
+char tandakutip = '\'';
+System.out.println(huruf_A+" dan "+huruf_A_juga);
+System.out.println(huruf_B+" dan "+huruf_B_juga);
+System.out.println(persen+" dan "+persen_juga);
+System.out.println(tandakutip);
+
+//FLOATING
+float short_pi = 3.14f;
+double long_pi = 3.141592653589793d;
+System.out.println("Nilai short pi = " + short_pi);
+System.out.println("Nilai tipe long pi = " + long_pi);
+
+//STRING
+String greeting = "Assalamualaikum dunia";
+    System.out.println(greeting);
+}
+
+
+
+
